69a27c4577bc106aa66667d0d92919cd4abf8675
Srinivas Kaza <srinivas@kaza.io>
2019-03-12 23:24:53 -0400

    Working renderer

--- tracer.py
+++ tracer.py
@@ -2,6 +2,7 @@
 
 import torch
 import numpy as np
+import cv2
 
 device = torch.device('cuda:0')
 
@@ -25,7 +26,7 @@ def projection(proj_matrix, view_matrix, width, height):
     rays[0, :, :] = origin + ray_vec * near
     rays[1, :, :] = ray_vec
 
-    return rays
+    return rays, origin
 
 
 def to_render_dist(dist):
@@ -57,22 +58,96 @@ def hit_mask(d):
                 hit[i, j] = 1.0
 
 
+def h(positions, index, sdf, EPS=1e-6):
+    forward = positions.clone()
+    backward = positions.clone()
+    forward[:, :, index] += EPS
+    backward[:, :, index] -= EPS
+    top = torch.stack((sdf(backward), sdf(positions), sdf(forward)), dim=-1)
+    bottom = torch.tensor((1.0, 2.0, 1.0)).view(1, -1, 1)
+
+    dim_0 = positions.shape[0]
+    dim_1 = positions.shape[1]
+    return torch.bmm(top.view(dim_0 * dim_1, 1, 3),
+                     bottom.expand(dim_0 * dim_1, 3, 1)).view(dim_0, dim_1)
+
+
+def h_p(positions, index, sdf, EPS=1e-6):
+    forward = positions.clone()
+    backward = positions.clone()
+    forward[:, :, index] += EPS
+    backward[:, :, index] -= EPS
+    top = torch.stack((sdf(backward), sdf(forward)), dim=-1)
+    bottom = torch.tensor((1.0, -1.0)).view(1, -1, 1)
+
+    dim_0 = positions.shape[0]
+    dim_1 = positions.shape[1]
+    return torch.bmm(top.view(dim_0 * dim_1, 1, 2),
+                     bottom.expand(dim_0 * dim_1, 2, 1)).view(dim_0, dim_1)
+
+
+def sobel(positions, m):
+    h_x = h_p(positions, 0, m) * h(positions, 1, m) * h(positions, 2, m)
+    h_y = h_p(positions, 1, m) * h(positions, 2, m) * h(positions, 0, m)
+    h_z = h_p(positions, 2, m) * h(positions, 0, m) * h(positions, 1, m)
+
+    h_all = torch.stack((h_x, h_y, h_z), -1)
+    return (-1.0 * h_all) / torch.norm(h_all, p=2, dim=-1).unsqueeze(-1)
+
+
+def light_source(light_color,
+                 positions,
+                 light_position,
+                 normals,
+                 kd=0.7,
+                 ks=0.3,
+                 ka=100.0):
+    dim_0 = positions.shape[0]
+    dim_1 = positions.shape[1]
+
+    light_vec = light_position - positions
+    light_vec /= torch.norm(light_vec, p=2, dim=-1).unsqueeze(-1)
+    ray_position = positions + light_vec
+    diffuse = kd * torch.clamp(
+        torch.bmm(normals.view(dim_0 * dim_1, 1, 3),
+                  light_vec.view(dim_0 * dim_1, 3, 1),
+                  ).view(dim_0, dim_1, 1) * light_color, 0.0, 1.0)
+    # TODO: Figure out specular
+    return diffuse
+
+
+def shade(rays, origin, EPS=1e-6):
+    normals = sobel(rays[0] - rays[1] * EPS, sdf_model)
+    cv2.imshow('shaded', normals.numpy())
+
+    top_light_color = torch.tensor((0.9, 0.9, 0.9))
+    self_light_color = torch.tensor((0.1, 0.1, 0.1))
+
+    top_light_pos = torch.tensor((10.0, 30.0, 0.0))
+    self_light_pos = origin
+
+    top_light = light_source(
+        top_light_color, rays[0], top_light_pos, normals)
+    self_light = light_source(
+        self_light_color, rays[0], self_light_pos, normals)
+    return top_light + self_light
+
+
 def forward_pass(grid_sdf, width=500, height=500):
-    import cv2
     projection_matrix = grid_sdf.perspective
     view_matrix = grid_sdf.view
-    rays = projection(projection_matrix, view_matrix, width, height)
+    rays, origin = projection(projection_matrix, view_matrix, width, height)
     for i in range(200):
         print("traced iteration {}".format(i))
         pos_2, d = sdf_iteration(rays, None)
         #print(pos_2[50:53, 50:53])
         #print(d[50:53, 50:53])
         rays[0] = pos_2
-    #print(torch.norm(rays[0, :, :, :3], p=2, dim=2) / 100.0)
-    cv2.imshow('rays', rays.numpy()[1, :, :, :3])
-    cv2.imshow('d', d.numpy() / 30.0)
-    #cv2.imshow('hit', hit)
-    cv2.waitKey(0)
+
+        cv2.imshow('rays', rays.numpy()[1])
+        cv2.imshow('d', d.numpy() / 30.0)
+        cv2.imshow('shaded', shade(rays, origin).numpy())
+        cv2.waitKey(50)
 
 
 if __name__ == '__main__':
