756b2e7d79a1a4dbb5c065604519589f4905c81b
Srinivas Kaza <srinivas@kaza.io>
2019-03-12 22:29:02 -0400

    tracer

--- tracer.py
+++ tracer.py
@@ -7,23 +7,23 @@ device = torch.device('cuda:0')
 
 
 def projection(proj_matrix, view_matrix, width, height):
-    rays = torch.zeros((2, width, height, 4), dtype=torch.float32)
+    rays = torch.zeros((2, width, height, 3), dtype=torch.float32)
     inv = torch.inverse(proj_matrix @ view_matrix)
-    origin = torch.inverse(view_matrix) @ torch.tensor((0.0, 0.0, 0.0, 1.0))
+    origin = (torch.inverse(view_matrix) @ torch.tensor(
+        (0.0, 0.0, 0.0, 1.0)))[:3]
     near = 0.1
-    for i in range(width):
-        for j in range(height):
-            clip_space = torch.tensor(((i / width) * 2.0 - 1.0,
-                                       (j / height) * 2.0 - 1.0,
-                                       1.0, 1.0))
-            tmp = inv @ clip_space
-            tmp /= tmp[3]
-            tmp -= torch.tensor((origin[0], origin[1], origin[2], 0.0),
-                                dtype=torch.float32)
-            ray_vec = tmp / torch.norm(tmp, p=2)
-
-            rays[0, i, j] = origin + ray_vec * near
-            rays[1, i, j] = ray_vec
+    grid = torch.meshgrid(torch.linspace(-1.0, 1.0, width),
+                          torch.linspace(-1.0, 1.0, height))
+    clip_space = torch.stack(
+        (grid[0], grid[1], torch.ones((width, height)), torch.ones((width, height))), dim=-1)
+    tmp = torch.matmul(inv, clip_space.view(width, height, 4, 1)).squeeze()
+    tmp /= tmp[:, :, 3:]
+    tmp = tmp[:, :, :3]
+    tmp -= torch.tensor((origin[0], origin[1], origin[2]),
+                        dtype=torch.float32)
+    ray_vec = tmp / torch.norm(tmp, p=2, dim=2).unsqueeze(-1)
+    rays[0, :, :] = origin + ray_vec * near
+    rays[1, :, :] = ray_vec
 
     return rays
 
@@ -49,24 +49,29 @@ def sdf_iteration(ray_matrix, model):
     return (xyz + to_render_dist(d) * vec, d)
 
 
-def forward_pass(grid_sdf, width=100, height=100):
+def hit_mask(d):
+    hit = np.zeros_like(d)
+    for i in range(d.shape[0]):
+        for j in range(d.shape[1]):
+            if d[i, j] < 1e-1:
+                hit[i, j] = 1.0
+
+
+def forward_pass(grid_sdf, width=500, height=500):
     import cv2
     projection_matrix = grid_sdf.perspective
     view_matrix = grid_sdf.view
     rays = projection(projection_matrix, view_matrix, width, height)
     for i in range(200):
+        print("traced iteration {}".format(i))
         pos_2, d = sdf_iteration(rays, None)
         #print(pos_2[50:53, 50:53])
         #print(d[50:53, 50:53])
         rays[0] = pos_2
     #print(torch.norm(rays[0, :, :, :3], p=2, dim=2) / 100.0)
-    hit = np.zeros_like(d)
-    for i in range(width):
-        for j in range(height):
-            if d[i, j] < 1e-1:
-                hit[i, j] = 1.0
     cv2.imshow('rays', rays.numpy()[1, :, :, :3])
-    cv2.imshow('hit', hit)
+    cv2.imshow('d', d.numpy() / 30.0)
+    #cv2.imshow('hit', hit)
     cv2.waitKey(0)
 
 
