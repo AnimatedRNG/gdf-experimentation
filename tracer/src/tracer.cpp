952684bfa01d4e9f325a5e06c4243e7d60e9c430
Srinivas Kaza <srinivas@kaza.io>
2019-05-29 15:30:57 -0400

    scheduling fixes?

--- tracer/src/tracer.cpp
+++ tracer/src/tracer.cpp
@@ -2,21 +2,36 @@
 
 #include "HalideBuffer.h"
 #include "halide_image_io.h"
+#include "matplotlibcpp.h"
 
 #include "tracer_render.h"
 
 using namespace Halide::Runtime;
 using namespace Halide::Tools;
 
+namespace plt = matplotlibcpp;
+
 int main() {
+    float projection_matrix[4][4] = {
+        {0.75, 0.0, 0.0,  0.0},
+        {0.0,  1.0, 0.0,  0.0},
+        {0.0,  0.0, 1.0,  0.0},
+        {0.0,  -0.2, 1.0,  0.0}
+    };
+
+    float view_matrix[4][4] = {
+        {-0.9825, 0.1422, 0.1206, 0.0},
+        {0.0, 0.6469, -0.7626, 0.0},
+        {0.1865, 0.7492, 0.6356, 0.0},
+        {-0.3166, 1.1503, 8.8977, 1.0}
+    };
+
+    Buffer<float> projection(projection_matrix);
+    Buffer<float> view(view_matrix);
+
     Buffer<float> output(4, 4, 1);
 
-    tracer_render(output);
+    tracer_render(projection, view, output);
 
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 4; j++) {
-            std::cout << output(i, j) << " ";
-        }
-        std::cout << std::endl;
-    }
+    //plt::show();
 }
--- tracer/src/tracer_generator.cpp
+++ tracer/src/tracer_generator.cpp
@@ -10,17 +10,31 @@ using namespace Halide;
 
 Var x("x"), y("y"), c("c");
 
+void apply_auto_schedule(Func F) {
+    std::map<std::string, Internal::Function> flist = Internal::find_transitive_calls(
+            F.function());
+    flist.insert(std::make_pair(F.name(), F.function()));
+    std::map<std::string, Internal::Function>::iterator fit;
+    for (fit = flist.begin(); fit != flist.end(); fit++) {
+        Func f(fit->second);
+        f.compute_root();
+         std::cout << "Warning: applying default schedule to " << f.name() << std::endl;
+    }
+    std::cout << std::endl;
+}
+
 class TracerGenerator : public Halide::Generator<TracerGenerator> {
   public:
 
-    //Input<Buffer<float>> in_{"in" , 2};
+    Input<Buffer<float>> projection_{"projection" , 2};
+    Input<Buffer<float>> view_{"view" , 2};
     Output<Buffer<float>> out_{"out", 3};
 
     Func projection(Func proj_matrix,
                     Func view_matrix,
                     size_t width,
                     size_t height,
-                    float near=0.1f) {
+                    float near = 0.1f) {
         Func rays("rays");
 
         Func ss_norm;
@@ -49,25 +63,25 @@ class TracerGenerator : public Halide::Generator<TracerGenerator> {
         homogeneous(x, y, c) = sum(viewproj_inv(c, k) *
                                    clip_space(x, y, k));
         homogeneous.bound(c, 0, 4).unroll(c);
+        viewproj_inv.compute_at(homogeneous, x);
 
         Func origin("origin");
         origin(c) = view_inv(c, 3);
-        origin.bound(c, 0, 3).unroll(c);
-
-        viewproj_inv.compute_at(homogeneous, x);
+        origin.bound(c, 0, 4).unroll(c);
 
         Func projected("projected");
         projected(x, y, c) = (homogeneous(x, y, c) / homogeneous(x, y, 3))
                              - origin(c);
         projected.bound(c, 0, 4).unroll(c);
+        homogeneous.compute_at(projected, x);
         origin.compute_at(projected, x);
 
         Func ray_vec("ray_vec");
         RDom norm_k(0, 3);
         // could use fast inverse sqrt, but not worth accuracy loss
         ray_vec(x, y, c) = projected(x, y, c) / Halide::sqrt(sum(
-                                  projected(x, y, norm_k) *
-                                  projected(x, y, norm_k)));
+                               projected(x, y, norm_k) *
+                               projected(x, y, norm_k)));
         ray_vec.bound(c, 0, 3).unroll(c);
         projected.compute_at(ray_vec, x);
 
@@ -78,6 +92,8 @@ class TracerGenerator : public Halide::Generator<TracerGenerator> {
         projection_result(x, y, c) = {ray_pos(x, y, c), ray_vec(x, y, c)};
         ray_vec.compute_at(projection_result, x);
         ray_pos.compute_at(projection_result, x);
+
+        apply_auto_schedule(projection_result);
 
         return projection_result;
     }
@@ -99,29 +115,16 @@ class TracerGenerator : public Halide::Generator<TracerGenerator> {
 
         a.for_each_element(identity(a));
 
-        a(0, 1) = 2.0f;
-        a(0, 2) = 3.0f;
-        a(0, 3) = 4.0f;
-
-        a(1, 2) = 2.0f;
-        a(1, 3) = 3.0f;
-        a(1, 0) = 4.0f;
-
-        a(2, 3) = 2.0f;
-        a(2, 0) = 3.0f;
-        a(2, 1) = 4.0f;
-
-        a(3, 0) = 2.0f;
-        a(3, 1) = 3.0f;
-        a(3, 2) = 4.0f;
-
         b.for_each_element(identity(b));
 
-        projection(Func(a), Func(b), 100, 100);
+        Func rays("rays");
+        rays = projection(Func(a), Func(b), 100, 100);
+        rays.compute_root();
 
-        out_(x, y, c) =
-            matmul::product(matmul::product(Func(b), 3.0f), Func(b))(x, y);
+        //out_(x, y, c) =
+        //    matmul::product(matmul::product(Func(b), 3.0f), Func(b))(x, y);
         //out_(x, y, c) = matmul::inverse(Func(a))(x, y);
+        out_(x, y, c) = rays(x, y, c)[0];
     }
 };
 
