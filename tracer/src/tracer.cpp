a3939ad27bf665fda009ee635ba3d6886abbce09
Srinivas Kaza <srinivas@kaza.io>
2019-05-29 17:43:06 -0400

    can easily and accurately get rays

--- tracer/src/tracer.cpp
+++ tracer/src/tracer.cpp
@@ -11,15 +11,142 @@ using namespace Halide::Tools;
 
 namespace plt = matplotlibcpp;
 
+bool gluInvertMatrix(const float mat[4][4], float invOut[4][4]) {
+    float inv[4][4], det;
+    int i;
+
+    inv[0][0] = mat[1][1]  * mat[2][2] * mat[3][3] -
+                mat[1][1]  * mat[3][2] * mat[2][3] -
+                mat[1][2]  * mat[2][1]  * mat[3][3] +
+                mat[1][2]  * mat[3][1]  * mat[2][3] +
+                mat[1][3] * mat[2][1]  * mat[3][2] -
+                mat[1][3] * mat[3][1]  * mat[2][2];
+
+    inv[0][1] = -mat[0][1]  * mat[2][2] * mat[3][3] +
+                mat[0][1]  * mat[3][2] * mat[2][3] +
+                mat[0][2]  * mat[2][1]  * mat[3][3] -
+                mat[0][2]  * mat[3][1]  * mat[2][3] -
+                mat[0][3] * mat[2][1]  * mat[3][2] +
+                mat[0][3] * mat[3][1]  * mat[2][2];
+
+    inv[0][2] = mat[0][1]  * mat[1][2] * mat[3][3] -
+                mat[0][1]  * mat[3][2] * mat[1][3] -
+                mat[0][2]  * mat[1][1] * mat[3][3] +
+                mat[0][2]  * mat[3][1] * mat[1][3] +
+                mat[0][3] * mat[1][1] * mat[3][2] -
+                mat[0][3] * mat[3][1] * mat[1][2];
+
+    inv[0][3] = -mat[0][1]  * mat[1][2] * mat[2][3] +
+                mat[0][1]  * mat[2][2] * mat[1][3] +
+                mat[0][2]  * mat[1][1] * mat[2][3] -
+                mat[0][2]  * mat[2][1] * mat[1][3] -
+                mat[0][3] * mat[1][1] * mat[2][2] +
+                mat[0][3] * mat[2][1] * mat[1][2];
+
+    inv[1][0] = -mat[1][0]  * mat[2][2] * mat[3][3] +
+                mat[1][0]  * mat[3][2] * mat[2][3] +
+                mat[1][2]  * mat[2][0] * mat[3][3] -
+                mat[1][2]  * mat[3][0] * mat[2][3] -
+                mat[1][3] * mat[2][0] * mat[3][2] +
+                mat[1][3] * mat[3][0] * mat[2][2];
+
+    inv[1][1] = mat[0][0]  * mat[2][2] * mat[3][3] -
+                mat[0][0]  * mat[3][2] * mat[2][3] -
+                mat[0][2]  * mat[2][0] * mat[3][3] +
+                mat[0][2]  * mat[3][0] * mat[2][3] +
+                mat[0][3] * mat[2][0] * mat[3][2] -
+                mat[0][3] * mat[3][0] * mat[2][2];
+
+    inv[1][2] = -mat[0][0]  * mat[1][2] * mat[3][3] +
+                mat[0][0]  * mat[3][2] * mat[1][3] +
+                mat[0][2]  * mat[1][0] * mat[3][3] -
+                mat[0][2]  * mat[3][0] * mat[1][3] -
+                mat[0][3] * mat[1][0] * mat[3][2] +
+                mat[0][3] * mat[3][0] * mat[1][2];
+
+    inv[1][3] = mat[0][0]  * mat[1][2] * mat[2][3] -
+                mat[0][0]  * mat[2][2] * mat[1][3] -
+                mat[0][2]  * mat[1][0] * mat[2][3] +
+                mat[0][2]  * mat[2][0] * mat[1][3] +
+                mat[0][3] * mat[1][0] * mat[2][2] -
+                mat[0][3] * mat[2][0] * mat[1][2];
+
+    inv[2][0] = mat[1][0]  * mat[2][1] * mat[3][3] -
+                mat[1][0]  * mat[3][1] * mat[2][3] -
+                mat[1][1]  * mat[2][0] * mat[3][3] +
+                mat[1][1]  * mat[3][0] * mat[2][3] +
+                mat[1][3] * mat[2][0] * mat[3][1] -
+                mat[1][3] * mat[3][0] * mat[2][1];
+
+    inv[2][1] = -mat[0][0]  * mat[2][1] * mat[3][3] +
+                mat[0][0]  * mat[3][1] * mat[2][3] +
+                mat[0][1]  * mat[2][0] * mat[3][3] -
+                mat[0][1]  * mat[3][0] * mat[2][3] -
+                mat[0][3] * mat[2][0] * mat[3][1] +
+                mat[0][3] * mat[3][0] * mat[2][1];
+
+    inv[2][2] = mat[0][0]  * mat[1][1] * mat[3][3] -
+                mat[0][0]  * mat[3][1] * mat[1][3] -
+                mat[0][1]  * mat[1][0] * mat[3][3] +
+                mat[0][1]  * mat[3][0] * mat[1][3] +
+                mat[0][3] * mat[1][0] * mat[3][1] -
+                mat[0][3] * mat[3][0] * mat[1][1];
+
+    inv[2][3] = -mat[0][0]  * mat[1][1] * mat[2][3] +
+                mat[0][0]  * mat[2][1] * mat[1][3] +
+                mat[0][1]  * mat[1][0] * mat[2][3] -
+                mat[0][1]  * mat[2][0] * mat[1][3] -
+                mat[0][3] * mat[1][0] * mat[2][1] +
+                mat[0][3] * mat[2][0] * mat[1][1];
+
+    inv[3][0] = -mat[1][0] * mat[2][1] * mat[3][2] +
+                mat[1][0] * mat[3][1] * mat[2][2] +
+                mat[1][1] * mat[2][0] * mat[3][2] -
+                mat[1][1] * mat[3][0] * mat[2][2] -
+                mat[1][2] * mat[2][0] * mat[3][1] +
+                mat[1][2] * mat[3][0] * mat[2][1];
+
+    inv[3][1] = mat[0][0] * mat[2][1] * mat[3][2] -
+                mat[0][0] * mat[3][1] * mat[2][2] -
+                mat[0][1] * mat[2][0] * mat[3][2] +
+                mat[0][1] * mat[3][0] * mat[2][2] +
+                mat[0][2] * mat[2][0] * mat[3][1] -
+                mat[0][2] * mat[3][0] * mat[2][1];
+
+    inv[3][2] = -mat[0][0] * mat[1][1] * mat[3][2] +
+                mat[0][0] * mat[3][1] * mat[1][2] +
+                mat[0][1] * mat[1][0] * mat[3][2] -
+                mat[0][1] * mat[3][0] * mat[1][2] -
+                mat[0][2] * mat[1][0] * mat[3][1] +
+                mat[0][2] * mat[3][0] * mat[1][1];
+
+    inv[3][3] = mat[0][0] * mat[1][1] * mat[2][2] -
+                mat[0][0] * mat[2][1] * mat[1][2] -
+                mat[0][1] * mat[1][0] * mat[2][2] +
+                mat[0][1] * mat[2][0] * mat[1][2] +
+                mat[0][2] * mat[1][0] * mat[2][1] -
+                mat[0][2] * mat[2][0] * mat[1][1];
+
+    det = 1.0f / (mat[0][0] * inv[0][0] +
+                  mat[1][0] * inv[0][1] +
+                  mat[2][0] * inv[0][2] +
+                  mat[3][0] * inv[0][3]);
+
+    for (i = 0; i < 16; i++)
+        invOut[i % 4][i / 4] = inv[i % 4][i / 4] * det;
+
+    return true;
+}
+
 int main() {
-    float projection_matrix[4][4] = {
+    const float projection_matrix[4][4] = {
         {0.75, 0.0, 0.0,  0.0},
         {0.0,  1.0, 0.0,  0.0},
-        {0.0,  0.0, 1.0,  0.0},
-        {0.0,  -0.2, 1.0,  0.0}
+        {0.0,  0.0, 1.0002,  1.0},
+        {0.0,  0.0, -0.2,  0.0}
     };
 
-    float view_matrix[4][4] = {
+    const float view_matrix[4][4] = {
         {-0.9825, 0.1422, 0.1206, 0.0},
         {0.0, 0.6469, -0.7626, 0.0},
         {0.1865, 0.7492, 0.6356, 0.0},
@@ -28,10 +155,41 @@ int main() {
 
     Buffer<float> projection(projection_matrix);
     Buffer<float> view(view_matrix);
+    Buffer<float> output(200, 200, 4);
+    Buffer<float> debug(4, 4);
 
-    Buffer<float> output(4, 4, 1);
+    tracer_render(projection, view, 200, 200, output, debug);
 
-    tracer_render(projection, view, output);
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            std::cout << debug(i, j) << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            std::cout
+                    << "("
+                    << output(i, j, 0) << ", "
+                    << output(i, j, 1) << ", "
+                    << output(i, j, 2) << ", "
+                    << output(i, j, 3) << ") ";
+        }
+        std::cout << std::endl;
+    }
+
+    /*float op[4][4];
+    gluInvertMatrix(view_matrix, op);
+
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            std::cout << op[j][i] << " ";
+        }
+        std::cout << std::endl;
+        }*/
+
+    convert_and_save_image(output, "test.png");
 
     //plt::show();
 }
--- tracer/src/tracer_generator.cpp
+++ tracer/src/tracer_generator.cpp
@@ -11,14 +11,15 @@ using namespace Halide;
 Var x("x"), y("y"), c("c");
 
 void apply_auto_schedule(Func F) {
-    std::map<std::string, Internal::Function> flist = Internal::find_transitive_calls(
+    std::map<std::string, Internal::Function> flist =
+        Internal::find_transitive_calls(
             F.function());
     flist.insert(std::make_pair(F.name(), F.function()));
     std::map<std::string, Internal::Function>::iterator fit;
     for (fit = flist.begin(); fit != flist.end(); fit++) {
         Func f(fit->second);
         f.compute_root();
-         std::cout << "Warning: applying default schedule to " << f.name() << std::endl;
+        std::cout << "Warning: applying default schedule to " << f.name() << std::endl;
     }
     std::cout << std::endl;
 }
@@ -26,29 +27,29 @@ void apply_auto_schedule(Func F) {
 class TracerGenerator : public Halide::Generator<TracerGenerator> {
   public:
 
-    Input<Buffer<float>> projection_{"projection" , 2};
-    Input<Buffer<float>> view_{"view" , 2};
+    Input<Buffer<float>> projection_{"projection", 2};
+    Input<Buffer<float>> view_{"view", 2};
+    Input<int32_t> width{"width"};
+    Input<int32_t> height{"height"};
     Output<Buffer<float>> out_{"out", 3};
+    Output<Buffer<float>> debug_{"deug", 2};
 
     Func projection(Func proj_matrix,
                     Func view_matrix,
-                    size_t width,
-                    size_t height,
                     float near = 0.1f) {
         Func rays("rays");
 
         Func ss_norm;
         ss_norm(x, y) = {
-            cast<float>(x) / ((float) width),
-            cast<float>(y) / ((float) height)
+            cast<float>(x) / (cast<float>(width)),
+            cast<float>(y) / (cast<float>(height))
         };
 
         Func clip_space("clip_space");
         clip_space(x, y, c) = 1.0f;
         clip_space(x, y, 0) = ss_norm(x, y)[0] * 2.0f - 1.0f;
         clip_space(x, y, 1) = ss_norm(x, y)[1] * 2.0f - 1.0f;
-        clip_space.bound(x, 0, 4).unroll(x);
-        clip_space.bound(y, 0, 4).unroll(y);
+        clip_space.bound(c, 0, 4).unroll(c);
 
         Func viewproj_inv("viewproj_inv");
         viewproj_inv =
@@ -57,6 +58,7 @@ class TracerGenerator : public Halide::Generator<TracerGenerator> {
         Func view_inv("view_inv");
         view_inv =
             matmul::inverse(view_matrix);
+        debug_(x, y) = view_inv(x, y);
 
         RDom k(0, 4);
         Func homogeneous("homogeneous");
@@ -82,7 +84,7 @@ class TracerGenerator : public Halide::Generator<TracerGenerator> {
         ray_vec(x, y, c) = projected(x, y, c) / Halide::sqrt(sum(
                                projected(x, y, norm_k) *
                                projected(x, y, norm_k)));
-        ray_vec.bound(c, 0, 3).unroll(c);
+        ray_vec.bound(c, 0, 4).unroll(c);
         projected.compute_at(ray_vec, x);
 
         Func ray_pos("ray_pos");
@@ -99,32 +101,15 @@ class TracerGenerator : public Halide::Generator<TracerGenerator> {
     }
 
     void generate() {
-
-        Buffer<float> a(4, 4);
-        Buffer<float> b(4, 4);
-
-        auto identity = [](Buffer<float>& a) {
-            return [&a](int x, int y) {
-                if (x == y) {
-                    a(x, y) = 1.0f;
-                } else {
-                    a(x, y) = 0.0;
-                }
-            };
-        };
-
-        a.for_each_element(identity(a));
-
-        b.for_each_element(identity(b));
-
         Func rays("rays");
-        rays = projection(Func(a), Func(b), 100, 100);
+        rays = projection(projection_, view_);
         rays.compute_root();
 
         //out_(x, y, c) =
         //    matmul::product(matmul::product(Func(b), 3.0f), Func(b))(x, y);
         //out_(x, y, c) = matmul::inverse(Func(a))(x, y);
-        out_(x, y, c) = rays(x, y, c)[0];
+        out_(x, y, c) = clamp(rays(x, y, 2 - c)[1], 0.0f, 1.0f);
+        //out_(x, y, c) = rays(x, y, c)[1];
     }
 };
 
