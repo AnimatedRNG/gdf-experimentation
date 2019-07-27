a0b2e5015636a8c7dcbd2d0af699b924ef3d59b0
Srinivas Kaza <srinivas@kaza.io>
2019-07-27 12:56:02 -0400

    working halide FMM generator, I think

--- tracer/src/fmm_generator.cpp
+++ tracer/src/fmm_generator.cpp
@@ -15,6 +15,8 @@ using namespace Halide;
 class FMMGenerator : public Halide::Generator<FMMGenerator> {
   public:
     Input<Func> sdf_{"sdf", Float(32), 3};
+    Input<Func> p0_{"p0", Float(32), 1};
+    Input<Func> p1_{"p1", Float(32), 1};
     Input<int32_t> d0{"d0"};
     Input<int32_t> d1{"d1"};
     Input<int32_t> d2{"d2"};
@@ -25,14 +27,48 @@ class FMMGenerator : public Halide::Generator<FMMGenerator> {
         return select(a < 0.0f, -1.0f, 1.0f);
     }
 
+    Expr abs_min(Tuple a) {
+        // abs min value, min value
+        Tuple tmp = {1e20f, 1e20f};
+        for (const Expr& e : a.as_vector()) {
+            tmp[0] = select(abs(e) <= tmp[0], abs(e), tmp[0]);
+            tmp[1] = select(abs(e) <= tmp[0], e, tmp[1]);
+        }
+        return tmp[1];
+    }
+
     void generate() {
         Func clamped = BoundaryConditions::repeat_edge(sdf_, {{0, d0}, {0, d1}, {0, d2}});
 
         Func sdf_iterative("sdf_iterative");
         sdf_iterative(x, y, z, t) = 1e10f;
 
+        TupleVec<3> p0({p0_(0), p0_(1), p0_(2)});
+        TupleVec<3> p1({p1_(0), p1_(1), p1_(2)});
+
+        TupleVec<3> pd = abs(p1 - p0);
+        TupleVec<3> us = pd / TupleVec<3>({
+            cast<float>(d0),
+            cast<float>(d1),
+            cast<float>(d2)
+        });
+
+        Var x1, y1, z1;
+        Func ds("ds");
+        // maps from the corner of the voxel to the other corners
+        ds(x1, y1, z1) = 0.0f;
+
+        for (int i1 = 0; i1 <= 1; i1++) {
+            for (int j1 = 0; j1 <= 1; j1++) {
+                for (int k1 = 0; k1 <= 1; k1++) {
+                    TupleVec<3> p({us[0] * i1, us[1] * j1, us[2] * k1});
+                    ds(i1, j1, k1) = norm(p);
+                }
+            }
+        }
+
         // neighborhood around each voxel
-        RDom r(-1, 2, -1, 2, -1, 2, "r");
+        RDom r(0, 3, 0, 3, 0, 3, "r");
 
         //Expr all_iters = max(max(d0, d1), d2);
         Expr all_iters = cast<int32_t>(Halide::sqrt(
@@ -40,10 +76,10 @@ class FMMGenerator : public Halide::Generator<FMMGenerator> {
         RDom it(0, all_iters);
 
         Func level_set_min("level_set_min");
-        level_set_min(x, y, z) = minimum(clamped(x + r.x, y + r.y, z + r.z));
+        level_set_min(x, y, z) = minimum(clamped(x + r.x - 1, y + r.y - 1, z + r.z - 1));
 
         Func level_set_max("level_set_max");
-        level_set_max(x, y, z) = maximum(clamped(x + r.x, y + r.y, z + r.z));
+        level_set_max(x, y, z) = maximum(clamped(x + r.x - 1, y + r.y - 1, z + r.z - 1));
 
         Func level_set("level_set");
         // level set involves a zero crossing
@@ -95,87 +131,79 @@ class FMMGenerator : public Halide::Generator<FMMGenerator> {
         next_iter(x, y, z, it) = select(considered(x, y, z, it + 1) !=
                                         considered(x, y, z, it), 1, 0);
 
-        sdf_iterative(x, y, z, 0) = cast<float>(level_set(x, y, z)) * clamped(x, y, z);
+        //sdf_iterative(x, y, z, 0) = cast<float>(level_set(x, y, z)) * clamped(x, y, z);
+        sdf_iterative(x, y, z, 0) = select(level_set(x, y, z) == 1,
+                                           clamped(x, y, z),
+                                           1e10f);
         sdf_iterative(rd.x, rd.y, rd.z, rd.w + 1) =
             select(next_iter(rd.x, rd.y, rd.z, rd.w) == 1,
-                   min(
-                       sdf_iterative(rd.x - 1, rd.y, rd.z, rd.w)
-                       + 1 * sign(sdf_iterative(rd.x - 1, rd.y, rd.z, rd.w)),
-                       sdf_iterative(rd.x - 1, rd.y - 1, rd.z, rd.w)
-                       + SQRT_2 * sign(sdf_iterative(rd.x - 1, rd.y - 1, rd.z, rd.w)),
-                       sdf_iterative(rd.x - 1, rd.y + 1, rd.z, rd.w)
-                       + SQRT_2 * sign(sdf_iterative(rd.x - 1, rd.y + 1, rd.z, rd.w)),
-                       sdf_iterative(rd.x - 1, rd.y, rd.z - 1, rd.w)
-                       + SQRT_2 * sign(sdf_iterative(rd.x - 1, rd.y, rd.z - 1, rd.w)),
-                       sdf_iterative(rd.x - 1, rd.y, rd.z + 1, rd.w)
-                       + SQRT_2 * sign(sdf_iterative(rd.x - 1, rd.y, rd.z + 1, rd.w)),
-                       sdf_iterative(rd.x - 1, rd.y - 1, rd.z - 1, rd.w)
-                       + SQRT_3 * sign(sdf_iterative(rd.x - 1, rd.y - 1, rd.z - 1, rd.w)),
-                       sdf_iterative(rd.x - 1, rd.y - 1, rd.z + 1, rd.w)
-                       + SQRT_3 * sign(sdf_iterative(rd.x - 1, rd.y - 1, rd.z + 1, rd.w)),
-                       sdf_iterative(rd.x - 1, rd.y + 1, rd.z - 1, rd.w)
-                       + SQRT_3 * sign(sdf_iterative(rd.x - 1, rd.y + 1, rd.z - 1, rd.w)),
-                       sdf_iterative(rd.x - 1, rd.y + 1, rd.z + 1, rd.w)
-                       + SQRT_3 * sign(sdf_iterative(rd.x - 1, rd.y + 1, rd.z + 1, rd.w)),
+                   abs_min(
+                       Tuple(
+                           sdf_iterative(rd.x - 1, rd.y, rd.z, rd.w)
+                           + ds(1, 0, 0) * sign(sdf_iterative(rd.x - 1, rd.y, rd.z, rd.w)),
+                           sdf_iterative(rd.x - 1, rd.y - 1, rd.z, rd.w)
+                           + ds(1, 1, 0) * sign(sdf_iterative(rd.x - 1, rd.y - 1, rd.z, rd.w)),
+                           sdf_iterative(rd.x - 1, rd.y + 1, rd.z, rd.w)
+                           + ds(1, 1, 0) * sign(sdf_iterative(rd.x - 1, rd.y + 1, rd.z, rd.w)),
+                           sdf_iterative(rd.x - 1, rd.y, rd.z - 1, rd.w)
+                           + ds(1, 0, 1) * sign(sdf_iterative(rd.x - 1, rd.y, rd.z - 1, rd.w)),
+                           sdf_iterative(rd.x - 1, rd.y, rd.z + 1, rd.w)
+                           + ds(1, 0, 1) * sign(sdf_iterative(rd.x - 1, rd.y, rd.z + 1, rd.w)),
+                           sdf_iterative(rd.x - 1, rd.y - 1, rd.z - 1, rd.w)
+                           + ds(1, 1, 1) * sign(sdf_iterative(rd.x - 1, rd.y - 1, rd.z - 1, rd.w)),
+                           sdf_iterative(rd.x - 1, rd.y - 1, rd.z + 1, rd.w)
+                           + ds(1, 1, 1) * sign(sdf_iterative(rd.x - 1, rd.y - 1, rd.z + 1, rd.w)),
+                           sdf_iterative(rd.x - 1, rd.y + 1, rd.z - 1, rd.w)
+                           + ds(1, 1, 1) * sign(sdf_iterative(rd.x - 1, rd.y + 1, rd.z - 1, rd.w)),
+                           sdf_iterative(rd.x - 1, rd.y + 1, rd.z + 1, rd.w)
+                           + ds(1, 1, 1) * sign(sdf_iterative(rd.x - 1, rd.y + 1, rd.z + 1, rd.w)),
 
-                       sdf_iterative(rd.x, rd.y - 1, rd.z, rd.w)
-                       + 1 * sign(sdf_iterative(rd.x, rd.y - 1, rd.z, rd.w)),
-                       sdf_iterative(rd.x, rd.y + 1, rd.z, rd.w)
-                       + 1 * sign(sdf_iterative(rd.x, rd.y + 1, rd.z, rd.w)),
-                       sdf_iterative(rd.x, rd.y, rd.z - 1, rd.w)
-                       + 1 * sign(sdf_iterative(rd.x, rd.y, rd.z - 1, rd.w)),
-                       sdf_iterative(rd.x, rd.y, rd.z + 1, rd.w)
-                       + 1 * sign(sdf_iterative(rd.x, rd.y, rd.z + 1, rd.w)),
-                       sdf_iterative(rd.x, rd.y - 1, rd.z - 1, rd.w)
-                       + SQRT_2 * sign(sdf_iterative(rd.x, rd.y - 1, rd.z - 1, rd.w)),
-                       sdf_iterative(rd.x, rd.y - 1, rd.z + 1, rd.w)
-                       + SQRT_2 * sign(sdf_iterative(rd.x, rd.y - 1, rd.z + 1, rd.w)),
-                       sdf_iterative(rd.x, rd.y + 1, rd.z - 1, rd.w)
-                       + SQRT_2 * sign(sdf_iterative(rd.x, rd.y + 1, rd.z - 1, rd.w)),
-                       sdf_iterative(rd.x, rd.y + 1, rd.z + 1, rd.w)
-                       + SQRT_2 * sign(sdf_iterative(rd.x, rd.y + 1, rd.z + 1, rd.w)),
+                           sdf_iterative(rd.x, rd.y - 1, rd.z, rd.w)
+                           + ds(0, 1, 0) * sign(sdf_iterative(rd.x, rd.y - 1, rd.z, rd.w)),
+                           sdf_iterative(rd.x, rd.y + 1, rd.z, rd.w)
+                           + ds(0, 1, 0) * sign(sdf_iterative(rd.x, rd.y + 1, rd.z, rd.w)),
+                           sdf_iterative(rd.x, rd.y, rd.z - 1, rd.w)
+                           + ds(0, 0, 1) * sign(sdf_iterative(rd.x, rd.y, rd.z - 1, rd.w)),
+                           sdf_iterative(rd.x, rd.y, rd.z + 1, rd.w)
+                           + ds(0, 0, 1) * sign(sdf_iterative(rd.x, rd.y, rd.z + 1, rd.w)),
+                           sdf_iterative(rd.x, rd.y - 1, rd.z - 1, rd.w)
+                           + ds(0, 1, 1) * sign(sdf_iterative(rd.x, rd.y - 1, rd.z - 1, rd.w)),
+                           sdf_iterative(rd.x, rd.y - 1, rd.z + 1, rd.w)
+                           + ds(0, 1, 1) * sign(sdf_iterative(rd.x, rd.y - 1, rd.z + 1, rd.w)),
+                           sdf_iterative(rd.x, rd.y + 1, rd.z - 1, rd.w)
+                           + ds(0, 1, 1) * sign(sdf_iterative(rd.x, rd.y + 1, rd.z - 1, rd.w)),
+                           sdf_iterative(rd.x, rd.y + 1, rd.z + 1, rd.w)
+                           + ds(0, 1, 1) * sign(sdf_iterative(rd.x, rd.y + 1, rd.z + 1, rd.w)),
 
-                       sdf_iterative(rd.x + 1, rd.y, rd.z, rd.w)
-                       + 1 * sign(sdf_iterative(rd.x + 1, rd.y, rd.z, rd.w)),
-                       sdf_iterative(rd.x + 1, rd.y - 1, rd.z, rd.w)
-                       + SQRT_2 * sign(sdf_iterative(rd.x + 1, rd.y - 1, rd.z, rd.w)),
-                       sdf_iterative(rd.x + 1, rd.y + 1, rd.z, rd.w)
-                       + SQRT_2 * sign(sdf_iterative(rd.x + 1, rd.y + 1, rd.z, rd.w)),
-                       sdf_iterative(rd.x + 1, rd.y, rd.z - 1, rd.w)
-                       + SQRT_2 * sign(sdf_iterative(rd.x + 1, rd.y, rd.z - 1, rd.w)),
-                       sdf_iterative(rd.x + 1, rd.y, rd.z + 1, rd.w)
-                       + SQRT_2 * sign(sdf_iterative(rd.x + 1, rd.y, rd.z + 1, rd.w)),
-                       sdf_iterative(rd.x + 1, rd.y - 1, rd.z - 1, rd.w)
-                       + SQRT_3 * sign(sdf_iterative(rd.x + 1, rd.y - 1, rd.z - 1, rd.w)),
-                       sdf_iterative(rd.x + 1, rd.y - 1, rd.z + 1, rd.w)
-                       + SQRT_3 * sign(sdf_iterative(rd.x + 1, rd.y - 1, rd.z + 1, rd.w)),
-                       sdf_iterative(rd.x + 1, rd.y + 1, rd.z - 1, rd.w)
-                       + SQRT_3 * sign(sdf_iterative(rd.x + 1, rd.y + 1, rd.z - 1, rd.w)),
-                       sdf_iterative(rd.x + 1, rd.y + 1, rd.z + 1, rd.w)
-                       + SQRT_3 * sign(sdf_iterative(rd.x + 1, rd.y + 1, rd.z + 1, rd.w))
+                           sdf_iterative(rd.x + 1, rd.y, rd.z, rd.w)
+                           + ds(1, 0, 0) * sign(sdf_iterative(rd.x + 1, rd.y, rd.z, rd.w)),
+                           sdf_iterative(rd.x + 1, rd.y - 1, rd.z, rd.w)
+                           + ds(1, 1, 0) * sign(sdf_iterative(rd.x + 1, rd.y - 1, rd.z, rd.w)),
+                           sdf_iterative(rd.x + 1, rd.y + 1, rd.z, rd.w)
+                           + ds(1, 1, 0) * sign(sdf_iterative(rd.x + 1, rd.y + 1, rd.z, rd.w)),
+                           sdf_iterative(rd.x + 1, rd.y, rd.z - 1, rd.w)
+                           + ds(1, 0, 1) * sign(sdf_iterative(rd.x + 1, rd.y, rd.z - 1, rd.w)),
+                           sdf_iterative(rd.x + 1, rd.y, rd.z + 1, rd.w)
+                           + ds(1, 0, 1) * sign(sdf_iterative(rd.x + 1, rd.y, rd.z + 1, rd.w)),
+                           sdf_iterative(rd.x + 1, rd.y - 1, rd.z - 1, rd.w)
+                           + ds(1, 1, 1) * sign(sdf_iterative(rd.x + 1, rd.y - 1, rd.z - 1, rd.w)),
+                           sdf_iterative(rd.x + 1, rd.y - 1, rd.z + 1, rd.w)
+                           + ds(1, 1, 1) * sign(sdf_iterative(rd.x + 1, rd.y - 1, rd.z + 1, rd.w)),
+                           sdf_iterative(rd.x + 1, rd.y + 1, rd.z - 1, rd.w)
+                           + ds(1, 1, 1) * sign(sdf_iterative(rd.x + 1, rd.y + 1, rd.z - 1, rd.w)),
+                           sdf_iterative(rd.x + 1, rd.y + 1, rd.z + 1, rd.w)
+                           + ds(1, 1, 1) * sign(sdf_iterative(rd.x + 1, rd.y + 1, rd.z + 1, rd.w))
+                       )
                    ),
                    sdf_iterative(rd.x, rd.y, rd.z, rd.w)
                   );
 
-        // we start with just the level set
-        /*sdf_iterative(x, y, z, 0) = cast<float>(level_set(x, y, z)) * sdf_(x, y, z);
-
-        // a tuple with the locations within the RDom which have the minimum value
-        Tuple sdf_neighbors = argmin(sdf_iterative(x + r.x, y + r.y, z + r.z, it));
-        // the distance to the nearest neighbor
-        Expr neighbor_dist = norm(sdf_neighbors);
-
-        // we only update when we are considering a node
-        sdf_iterative(x, y, z, it + 1) =
-            select(next_iter(x, y, z, it) == 1,
-                   neighbor_dist,
-                   sdf_iterative(x, y, z, it));
-
-                   sdf_output_(x, y, z) = sdf_iterative(x, y, z, all_iters);*/
         //sdf_output_(x, y, z) = cast<float>(considered(x, y, z, 2));
         //sdf_output_(x, y, z) = cast<float>(next_iter(x, y, z, 2));
-        //sdf_output_(x, y, z) = cast<float>(sdf_iterative(x, y, z, all_iters));
-        sdf_output_(x, y, z) = abs(clamped(x, y, z));
+        sdf_output_(x, y, z) = sdf_iterative(x, y, z, all_iters);
+        //sdf_output_(x, y, z) = max(0.0f, sdf_iterative(x, y, z, 4));
+        //sdf_output_(x, y, z) = cast<float>(level_set(x, y, z));
+        //sdf_output_(x, y, z) = clamped(x, y, z);
         apply_auto_schedule(sdf_output_);
 
         /*Halide::SimpleAutoscheduleOptions options;
--- tracer/src/fmm_test.cpp
+++ tracer/src/fmm_test.cpp
@@ -15,10 +15,11 @@ using namespace Halide::Tools;
 
 int main(int argc, char** argv) {
     const int32_t n_matrix[3] = {
-        128, 128, 128
+        32, 32, 32
     };
 
     Buffer<float> sdf(n_matrix[0], n_matrix[1], n_matrix[2]);
+    Buffer<float> sdf_output(n_matrix[0], n_matrix[1], n_matrix[2]);
     Buffer<float> p0(3);
     Buffer<float> p1(3);
 
@@ -28,8 +29,28 @@ int main(int argc, char** argv) {
     p0.set_host_dirty();
     p1.copy_to_device(halide_cuda_device_interface());
 
+    sdf_output.set_host_dirty();
+    sdf_output.copy_to_device(halide_cuda_device_interface());
+
     auto start = std::chrono::steady_clock::now();
     sdf_gen(n_matrix[0], n_matrix[1], n_matrix[2], sdf, p0, p1);
 
     auto end = std::chrono::steady_clock::now();
+
+    fmm_gen(sdf, p0, p1, n_matrix[0], n_matrix[1], n_matrix[2], sdf_output);
+
+    sdf_output.copy_to_host();
+
+    for (int i = 0; i < n_matrix[0]; i++) {
+        for (int j = 0; j < n_matrix[1]; j++) {
+            printf("%.2f\t", sdf_output(i, j, n_matrix[2] / 2));
+        }
+        std::cout << std::endl;
+    }
+
+    Buffer<float> n_64 = sdf_output.sliced(2, n_matrix[2] / 2);
+    convert_and_save_image(n_64, "n_" + std::to_string(n_matrix[2] / 2) + ".png");
+
+    Buffer<float> n_orig_64 = sdf.sliced(2, n_matrix[2] / 2);
+    convert_and_save_image(n_orig_64, "orig_n_" + std::to_string(n_matrix[2] / 2) + ".png");
 }
