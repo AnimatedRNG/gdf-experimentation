70b277940acfbe86014d84266124c51803fe4874
Srinivas Kaza <srinivas@kaza.io>
2019-07-27 11:20:31 -0400

    some progress on fmm_generator

--- tracer/src/fmm_generator.cpp
+++ tracer/src/fmm_generator.cpp
@@ -9,6 +9,9 @@
 
 using namespace Halide;
 
+#define SQRT_2 1.41421356f
+#define SQRT_3 1.73205080f
+
 class FMMGenerator : public Halide::Generator<FMMGenerator> {
   public:
     Input<Func> sdf_{"sdf", Float(32), 3};
@@ -17,6 +20,10 @@ class FMMGenerator : public Halide::Generator<FMMGenerator> {
     Input<int32_t> d2{"d2"};
 
     Output<Func> sdf_output_{"sdf_output", 3};
+
+    Expr sign(Expr a) {
+        return select(a < 0.0f, -1.0f, 1.0f);
+    }
 
     void generate() {
         Func clamped = BoundaryConditions::repeat_edge(sdf_, {{0, d0}, {0, d1}, {0, d2}});
@@ -27,14 +34,16 @@ class FMMGenerator : public Halide::Generator<FMMGenerator> {
         // neighborhood around each voxel
         RDom r(-1, 2, -1, 2, -1, 2, "r");
 
-        Expr all_iters = max(max(d0, d1), d2);
+        //Expr all_iters = max(max(d0, d1), d2);
+        Expr all_iters = cast<int32_t>(Halide::sqrt(
+                                           cast<float>(d0 * d0 + d1 * d1 + d2 * d2)));
         RDom it(0, all_iters);
 
         Func level_set_min("level_set_min");
-        level_set_min(x, y, z) = minimum(sdf_(x + r.x, y + r.y, z + r.z));
+        level_set_min(x, y, z) = minimum(clamped(x + r.x, y + r.y, z + r.z));
 
         Func level_set_max("level_set_max");
-        level_set_max(x, y, z) = maximum(sdf_(x + r.x, y + r.y, z + r.z));
+        level_set_max(x, y, z) = maximum(clamped(x + r.x, y + r.y, z + r.z));
 
         Func level_set("level_set");
         // level set involves a zero crossing
@@ -44,11 +53,41 @@ class FMMGenerator : public Halide::Generator<FMMGenerator> {
 
         // each iteration, we consider our neighbors
         Func considered("considered");
+        RDom rd(0, d0, 0, d1, 0, d2, 0, all_iters, "rd");
+
         considered(x, y, z, t) = level_set(x, y, z);
 
-        considered(x, y, z, it + 1) =
-            cast<int32_t>(
-            maximum(r, considered(x + r.x, y + r.y, z + r.z, it)));
+        considered(rd.x, rd.y, rd.z, rd.w + 1) =
+            cast<int32_t>(max(
+                              considered(rd.x - 1, rd.y, rd.z, rd.w),
+                              considered(rd.x - 1, rd.y - 1, rd.z, rd.w),
+                              considered(rd.x - 1, rd.y + 1, rd.z, rd.w),
+                              considered(rd.x - 1, rd.y, rd.z - 1, rd.w),
+                              considered(rd.x - 1, rd.y, rd.z + 1, rd.w),
+                              considered(rd.x - 1, rd.y - 1, rd.z - 1, rd.w),
+                              considered(rd.x - 1, rd.y - 1, rd.z + 1, rd.w),
+                              considered(rd.x - 1, rd.y + 1, rd.z - 1, rd.w),
+                              considered(rd.x - 1, rd.y + 1, rd.z + 1, rd.w),
+
+                              considered(rd.x, rd.y - 1, rd.z, rd.w),
+                              considered(rd.x, rd.y + 1, rd.z, rd.w),
+                              considered(rd.x, rd.y, rd.z - 1, rd.w),
+                              considered(rd.x, rd.y, rd.z + 1, rd.w),
+                              considered(rd.x, rd.y - 1, rd.z - 1, rd.w),
+                              considered(rd.x, rd.y - 1, rd.z + 1, rd.w),
+                              considered(rd.x, rd.y + 1, rd.z - 1, rd.w),
+                              considered(rd.x, rd.y + 1, rd.z + 1, rd.w),
+
+                              considered(rd.x + 1, rd.y, rd.z, rd.w),
+                              considered(rd.x + 1, rd.y - 1, rd.z, rd.w),
+                              considered(rd.x + 1, rd.y + 1, rd.z, rd.w),
+                              considered(rd.x + 1, rd.y, rd.z - 1, rd.w),
+                              considered(rd.x + 1, rd.y, rd.z + 1, rd.w),
+                              considered(rd.x + 1, rd.y - 1, rd.z - 1, rd.w),
+                              considered(rd.x + 1, rd.y - 1, rd.z + 1, rd.w),
+                              considered(rd.x + 1, rd.y + 1, rd.z - 1, rd.w),
+                              considered(rd.x + 1, rd.y + 1, rd.z + 1, rd.w)
+                          ));
 
         // select the cells which will be occupied next iteration
         Func next_iter("next_iter");
@@ -56,8 +95,70 @@ class FMMGenerator : public Halide::Generator<FMMGenerator> {
         next_iter(x, y, z, it) = select(considered(x, y, z, it + 1) !=
                                         considered(x, y, z, it), 1, 0);
 
+        sdf_iterative(x, y, z, 0) = cast<float>(level_set(x, y, z)) * clamped(x, y, z);
+        sdf_iterative(rd.x, rd.y, rd.z, rd.w + 1) =
+            select(next_iter(rd.x, rd.y, rd.z, rd.w) == 1,
+                   min(
+                       sdf_iterative(rd.x - 1, rd.y, rd.z, rd.w)
+                       + 1 * sign(sdf_iterative(rd.x - 1, rd.y, rd.z, rd.w)),
+                       sdf_iterative(rd.x - 1, rd.y - 1, rd.z, rd.w)
+                       + SQRT_2 * sign(sdf_iterative(rd.x - 1, rd.y - 1, rd.z, rd.w)),
+                       sdf_iterative(rd.x - 1, rd.y + 1, rd.z, rd.w)
+                       + SQRT_2 * sign(sdf_iterative(rd.x - 1, rd.y + 1, rd.z, rd.w)),
+                       sdf_iterative(rd.x - 1, rd.y, rd.z - 1, rd.w)
+                       + SQRT_2 * sign(sdf_iterative(rd.x - 1, rd.y, rd.z - 1, rd.w)),
+                       sdf_iterative(rd.x - 1, rd.y, rd.z + 1, rd.w)
+                       + SQRT_2 * sign(sdf_iterative(rd.x - 1, rd.y, rd.z + 1, rd.w)),
+                       sdf_iterative(rd.x - 1, rd.y - 1, rd.z - 1, rd.w)
+                       + SQRT_3 * sign(sdf_iterative(rd.x - 1, rd.y - 1, rd.z - 1, rd.w)),
+                       sdf_iterative(rd.x - 1, rd.y - 1, rd.z + 1, rd.w)
+                       + SQRT_3 * sign(sdf_iterative(rd.x - 1, rd.y - 1, rd.z + 1, rd.w)),
+                       sdf_iterative(rd.x - 1, rd.y + 1, rd.z - 1, rd.w)
+                       + SQRT_3 * sign(sdf_iterative(rd.x - 1, rd.y + 1, rd.z - 1, rd.w)),
+                       sdf_iterative(rd.x - 1, rd.y + 1, rd.z + 1, rd.w)
+                       + SQRT_3 * sign(sdf_iterative(rd.x - 1, rd.y + 1, rd.z + 1, rd.w)),
+
+                       sdf_iterative(rd.x, rd.y - 1, rd.z, rd.w)
+                       + 1 * sign(sdf_iterative(rd.x, rd.y - 1, rd.z, rd.w)),
+                       sdf_iterative(rd.x, rd.y + 1, rd.z, rd.w)
+                       + 1 * sign(sdf_iterative(rd.x, rd.y + 1, rd.z, rd.w)),
+                       sdf_iterative(rd.x, rd.y, rd.z - 1, rd.w)
+                       + 1 * sign(sdf_iterative(rd.x, rd.y, rd.z - 1, rd.w)),
+                       sdf_iterative(rd.x, rd.y, rd.z + 1, rd.w)
+                       + 1 * sign(sdf_iterative(rd.x, rd.y, rd.z + 1, rd.w)),
+                       sdf_iterative(rd.x, rd.y - 1, rd.z - 1, rd.w)
+                       + SQRT_2 * sign(sdf_iterative(rd.x, rd.y - 1, rd.z - 1, rd.w)),
+                       sdf_iterative(rd.x, rd.y - 1, rd.z + 1, rd.w)
+                       + SQRT_2 * sign(sdf_iterative(rd.x, rd.y - 1, rd.z + 1, rd.w)),
+                       sdf_iterative(rd.x, rd.y + 1, rd.z - 1, rd.w)
+                       + SQRT_2 * sign(sdf_iterative(rd.x, rd.y + 1, rd.z - 1, rd.w)),
+                       sdf_iterative(rd.x, rd.y + 1, rd.z + 1, rd.w)
+                       + SQRT_2 * sign(sdf_iterative(rd.x, rd.y + 1, rd.z + 1, rd.w)),
+
+                       sdf_iterative(rd.x + 1, rd.y, rd.z, rd.w)
+                       + 1 * sign(sdf_iterative(rd.x + 1, rd.y, rd.z, rd.w)),
+                       sdf_iterative(rd.x + 1, rd.y - 1, rd.z, rd.w)
+                       + SQRT_2 * sign(sdf_iterative(rd.x + 1, rd.y - 1, rd.z, rd.w)),
+                       sdf_iterative(rd.x + 1, rd.y + 1, rd.z, rd.w)
+                       + SQRT_2 * sign(sdf_iterative(rd.x + 1, rd.y + 1, rd.z, rd.w)),
+                       sdf_iterative(rd.x + 1, rd.y, rd.z - 1, rd.w)
+                       + SQRT_2 * sign(sdf_iterative(rd.x + 1, rd.y, rd.z - 1, rd.w)),
+                       sdf_iterative(rd.x + 1, rd.y, rd.z + 1, rd.w)
+                       + SQRT_2 * sign(sdf_iterative(rd.x + 1, rd.y, rd.z + 1, rd.w)),
+                       sdf_iterative(rd.x + 1, rd.y - 1, rd.z - 1, rd.w)
+                       + SQRT_3 * sign(sdf_iterative(rd.x + 1, rd.y - 1, rd.z - 1, rd.w)),
+                       sdf_iterative(rd.x + 1, rd.y - 1, rd.z + 1, rd.w)
+                       + SQRT_3 * sign(sdf_iterative(rd.x + 1, rd.y - 1, rd.z + 1, rd.w)),
+                       sdf_iterative(rd.x + 1, rd.y + 1, rd.z - 1, rd.w)
+                       + SQRT_3 * sign(sdf_iterative(rd.x + 1, rd.y + 1, rd.z - 1, rd.w)),
+                       sdf_iterative(rd.x + 1, rd.y + 1, rd.z + 1, rd.w)
+                       + SQRT_3 * sign(sdf_iterative(rd.x + 1, rd.y + 1, rd.z + 1, rd.w))
+                   ),
+                   sdf_iterative(rd.x, rd.y, rd.z, rd.w)
+                  );
+
         // we start with just the level set
-        sdf_iterative(x, y, z, 0) = cast<float>(level_set(x, y, z)) * sdf_(x, y, z);
+        /*sdf_iterative(x, y, z, 0) = cast<float>(level_set(x, y, z)) * sdf_(x, y, z);
 
         // a tuple with the locations within the RDom which have the minimum value
         Tuple sdf_neighbors = argmin(sdf_iterative(x + r.x, y + r.y, z + r.z, it));
@@ -70,9 +171,14 @@ class FMMGenerator : public Halide::Generator<FMMGenerator> {
                    neighbor_dist,
                    sdf_iterative(x, y, z, it));
 
-        sdf_output_(x, y, z) = sdf_iterative(x, y, z, all_iters);
+                   sdf_output_(x, y, z) = sdf_iterative(x, y, z, all_iters);*/
+        //sdf_output_(x, y, z) = cast<float>(considered(x, y, z, 2));
+        //sdf_output_(x, y, z) = cast<float>(next_iter(x, y, z, 2));
+        //sdf_output_(x, y, z) = cast<float>(sdf_iterative(x, y, z, all_iters));
+        sdf_output_(x, y, z) = abs(clamped(x, y, z));
+        apply_auto_schedule(sdf_output_);
 
-        Halide::SimpleAutoscheduleOptions options;
+        /*Halide::SimpleAutoscheduleOptions options;
         options.gpu = get_target().has_gpu_feature();
         options.gpu_tile_channel = 1;
         options.unroll_rvar_size = 128;
@@ -94,7 +200,7 @@ class FMMGenerator : public Halide::Generator<FMMGenerator> {
                 {0, 128}
             }
         },
-        options);
+        options);*/
     }
 
   private:
