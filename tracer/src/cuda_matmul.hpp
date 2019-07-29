0ad3157d0601ef30389fbf82c8f63ee089191479
Srinivas Kaza <srinivas@kaza.io>
2019-07-29 19:08:45 -0400

    cuda matrix/array utilities

---
+++ tracer/src/cuda_matmul.hpp
@@ -0,0 +1,307 @@
+#pragma once
+
+#include "math.h"
+
+template <typename T, size_t N>
+struct cuda_array {
+    size_t shape[N];
+    size_t stride[N];
+    size_t num_elements;
+    T* data;
+};
+
+template <typename T, size_t N>
+__host__ cuda_array<T, N>* create(size_t dims[N]) {
+    cuda_array<T, N>* arr = new cuda_array<T, N>;
+
+    size_t product = 1;
+    for (int i = 0; i < N; i++) {
+        arr->shape[i] = dims[i];
+        arr->stride[i] = product;
+
+        product *= dims[i];
+    }
+    arr->num_elements = product;
+    arr->data = new T[product];
+
+    return arr;
+}
+
+template <typename T, size_t N>
+__host__ __device__ void assign(cuda_array<T, N>* arr, T* ptr, size_t dims[N],
+                                const bool& delete_mem = false) {
+    size_t product = 1;
+    for (int i = 0; i < N; i++) {
+        arr->shape[i] = dims[i];
+        arr->stride[i] = product;
+
+        product *= dims[i];
+    }
+    arr->num_elements = product;
+
+    if (delete_mem) {
+        delete[] arr->data;
+    }
+
+    arr->data = ptr;
+}
+
+template <typename T, size_t N>
+__host__ void delete_array(cuda_array<T, N>* array) {
+    delete[] array->data;
+    delete array;
+}
+
+template <typename T, size_t N>
+__host__ __device__ T& index(cuda_array<T, N>* arr, const int& i) {
+    return arr->data[i * arr->stride[0]];
+}
+
+template <typename T, size_t N>
+__host__ __device__ T& index(cuda_array<T, N>* arr,
+                             const int& i,
+                             const int& j) {
+    return arr->data[i * arr->stride[0] + j * arr->stride[1]];
+}
+
+template <typename T, size_t N>
+__host__ __device__ T& index(cuda_array<T, N>* arr,
+                             const int& i,
+                             const int& j,
+                             const int& k) {
+    return arr->data[i * arr->stride[0] + j * arr->stride[1] + k * arr->stride[2]];
+}
+
+float example_sphere(float x, float y, float z) {
+    return sqrtf(x * x + y * y + z * z) - 3.0f;
+}
+
+template <typename T>
+__host__ void fill(cuda_array<T, 2>* arr, const T& val) {
+    for (int j = 0; j < arr->shape[1]; j++) {
+        for (int i = 0; i < arr->shape[0]; i++) {
+            index(arr, i, j) = val;
+        }
+    }
+}
+
+template <typename T>
+__host__ void fill(cuda_array<T, 3>* arr, const T& val) {
+    for (int k = 0; k < arr->shape[2]; k++) {
+        for (int j = 0; j < arr->shape[1]; j++) {
+            for (int i = 0; i < arr->shape[0]; i++) {
+                index(arr, i, j, k) = val;
+            }
+        }
+    }
+}
+
+template <typename T>
+__host__ void gen_sdf(std::function<float(float, float, float)> func,
+                      cuda_array<T, 3>* arr) {
+    for (int k = 0; k < arr->shape[2]; k++) {
+        for (int j = 0; j < arr->shape[1]; j++) {
+            for (int i = 0; i < arr->shape[0]; i++) {
+                index(arr, i, j, k) = func(i, j, k);
+            }
+        }
+    }
+}
+
+template <typename T>
+__host__ void print(cuda_array<T, 2>* arr) {
+    for (int i = 0; i < arr->shape[0]; i++) {
+        for (int j = 0; j < arr->shape[1]; j++) {
+            std::cout << index(arr, i, j) << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+template <typename T, size_t N>
+__host__ T* to_device(const cuda_array<T, N>* arr, size_t** shape) {
+    T* device_ptr;
+
+    std::cout << "copying " << arr->num_elements << " to device" << std::endl;
+    cudaMalloc(&device_ptr, arr->num_elements * sizeof(T));
+    cudaMemcpy(device_ptr, arr->data, arr->num_elements * sizeof(T),
+               cudaMemcpyHostToDevice);
+
+    cudaMalloc(shape, N * sizeof(size_t));
+    cudaMemcpy(*shape, arr->shape, N * sizeof(size_t),
+               cudaMemcpyHostToDevice);
+
+    return device_ptr;
+}
+
+template <typename T, size_t N>
+__host__ void to_host(T* arr, cuda_array<T, N>* host) {
+    std::cout << "copying " << host->num_elements << " to host" << std::endl;
+
+    int ret = cudaMemcpy(host->data, arr,
+                         host->num_elements * sizeof(T),
+                         cudaMemcpyDeviceToHost);
+    std::cout << "ret " << ret << std::endl;
+    cudaFree((void*) arr);
+}
+
+// from MESA GLU
+__host__ __device__ bool invert_matrix(float* m, float* invOut) {
+    float inv[16], det;
+    int i;
+
+    inv[0] = m[5]  * m[10] * m[15] -
+             m[5]  * m[11] * m[14] -
+             m[9]  * m[6]  * m[15] +
+             m[9]  * m[7]  * m[14] +
+             m[13] * m[6]  * m[11] -
+             m[13] * m[7]  * m[10];
+
+    inv[4] = -m[4]  * m[10] * m[15] +
+             m[4]  * m[11] * m[14] +
+             m[8]  * m[6]  * m[15] -
+             m[8]  * m[7]  * m[14] -
+             m[12] * m[6]  * m[11] +
+             m[12] * m[7]  * m[10];
+
+    inv[8] = m[4]  * m[9] * m[15] -
+             m[4]  * m[11] * m[13] -
+             m[8]  * m[5] * m[15] +
+             m[8]  * m[7] * m[13] +
+             m[12] * m[5] * m[11] -
+             m[12] * m[7] * m[9];
+
+    inv[12] = -m[4]  * m[9] * m[14] +
+              m[4]  * m[10] * m[13] +
+              m[8]  * m[5] * m[14] -
+              m[8]  * m[6] * m[13] -
+              m[12] * m[5] * m[10] +
+              m[12] * m[6] * m[9];
+
+    inv[1] = -m[1]  * m[10] * m[15] +
+             m[1]  * m[11] * m[14] +
+             m[9]  * m[2] * m[15] -
+             m[9]  * m[3] * m[14] -
+             m[13] * m[2] * m[11] +
+             m[13] * m[3] * m[10];
+
+    inv[5] = m[0]  * m[10] * m[15] -
+             m[0]  * m[11] * m[14] -
+             m[8]  * m[2] * m[15] +
+             m[8]  * m[3] * m[14] +
+             m[12] * m[2] * m[11] -
+             m[12] * m[3] * m[10];
+
+    inv[9] = -m[0]  * m[9] * m[15] +
+             m[0]  * m[11] * m[13] +
+             m[8]  * m[1] * m[15] -
+             m[8]  * m[3] * m[13] -
+             m[12] * m[1] * m[11] +
+             m[12] * m[3] * m[9];
+
+    inv[13] = m[0]  * m[9] * m[14] -
+              m[0]  * m[10] * m[13] -
+              m[8]  * m[1] * m[14] +
+              m[8]  * m[2] * m[13] +
+              m[12] * m[1] * m[10] -
+              m[12] * m[2] * m[9];
+
+    inv[2] = m[1]  * m[6] * m[15] -
+             m[1]  * m[7] * m[14] -
+             m[5]  * m[2] * m[15] +
+             m[5]  * m[3] * m[14] +
+             m[13] * m[2] * m[7] -
+             m[13] * m[3] * m[6];
+
+    inv[6] = -m[0]  * m[6] * m[15] +
+             m[0]  * m[7] * m[14] +
+             m[4]  * m[2] * m[15] -
+             m[4]  * m[3] * m[14] -
+             m[12] * m[2] * m[7] +
+             m[12] * m[3] * m[6];
+
+    inv[10] = m[0]  * m[5] * m[15] -
+              m[0]  * m[7] * m[13] -
+              m[4]  * m[1] * m[15] +
+              m[4]  * m[3] * m[13] +
+              m[12] * m[1] * m[7] -
+              m[12] * m[3] * m[5];
+
+    inv[14] = -m[0]  * m[5] * m[14] +
+              m[0]  * m[6] * m[13] +
+              m[4]  * m[1] * m[14] -
+              m[4]  * m[2] * m[13] -
+              m[12] * m[1] * m[6] +
+              m[12] * m[2] * m[5];
+
+    inv[3] = -m[1] * m[6] * m[11] +
+             m[1] * m[7] * m[10] +
+             m[5] * m[2] * m[11] -
+             m[5] * m[3] * m[10] -
+             m[9] * m[2] * m[7] +
+             m[9] * m[3] * m[6];
+
+    inv[7] = m[0] * m[6] * m[11] -
+             m[0] * m[7] * m[10] -
+             m[4] * m[2] * m[11] +
+             m[4] * m[3] * m[10] +
+             m[8] * m[2] * m[7] -
+             m[8] * m[3] * m[6];
+
+    inv[11] = -m[0] * m[5] * m[11] +
+              m[0] * m[7] * m[9] +
+              m[4] * m[1] * m[11] -
+              m[4] * m[3] * m[9] -
+              m[8] * m[1] * m[7] +
+              m[8] * m[3] * m[5];
+
+    inv[15] = m[0] * m[5] * m[10] -
+              m[0] * m[6] * m[9] -
+              m[4] * m[1] * m[10] +
+              m[4] * m[2] * m[9] +
+              m[8] * m[1] * m[6] -
+              m[8] * m[2] * m[5];
+
+    det = m[0] * inv[0] + m[1] * inv[4] + m[2] * inv[8] + m[3] * inv[12];
+
+    if (det == 0)
+        return false;
+
+    det = 1.0 / det;
+
+    for (i = 0; i < 16; i++)
+        invOut[i] = inv[i] * det;
+
+    return true;
+}
+
+template <typename T>
+__host__ __device__ void mat4(cuda_array<T, 2>* m, T data[4][4]) {
+    size_t dims[2] = {4, 4};
+    assign<float, 2>(m, (T*) data, dims);
+}
+
+template <typename T, size_t N>
+__host__ __device__ void matmul_sq(cuda_array<T, N>* a, cuda_array<T, N>* b, cuda_array<T, N>* c) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            for (int k = 0; k < N; k++) {
+                index(c, i, j) = index(a, i, k) * index(b, k, j);
+            }
+        }
+    }
+}
+
+template <typename T>
+__host__ __device__ float3 matvec(cuda_array<T, 3>* a, const float3& b) {
+    return make_float3(index(a, 0, 0) * b.x + index(a, 0, 1) * b.y + index(a, 0, 2) * b.z,
+                       index(a, 1, 0) * b.x + index(a, 1, 1) * b.y + index(a, 1, 2) * b.z,
+                       index(a, 2, 0) * b.x + index(a, 2, 1) * b.y + index(a, 2, 2) * b.z);
+}
+
+template <typename T>
+__host__ __device__ float3 matvec(cuda_array<T, 4>* a, const float3& b) {
+    return make_float3(index(a, 0, 0) * b.x + index(a, 0, 1) * b.y + index(a, 0, 2) * b.z,
+                       index(a, 1, 0) * b.x + index(a, 1, 1) * b.y + index(a, 1, 2) * b.z,
+                       index(a, 2, 0) * b.x + index(a, 2, 1) * b.y + index(a, 2, 2) * b.z);
+}
