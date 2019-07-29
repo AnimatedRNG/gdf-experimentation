741bee340173af5e24b9dc72883f4180173b4b91
Srinivas Kaza <srinivas@kaza.io>
2019-07-29 19:02:21 -0400

    some of the projection code

--- tracer/src/tracer.cu
+++ tracer/src/tracer.cu
@@ -1,148 +1,85 @@
 #include <iostream>
 #include <functional>
 #include "math.h"
+#include "helper_math.h"
 
-template <typename T, size_t N>
-struct cuda_array {
-    size_t shape[N];
-    size_t stride[N];
-    size_t num_elements;
-    T* data;
-};
+#include "cuda_matmul.hpp"
 
-template <typename T, size_t N>
-__host__ cuda_array<T, N>* create(size_t dims[N]) {
-    cuda_array<T, N>* arr = new cuda_array<T, N>;
+__device__ void projection_gen(int x,
+                               int y,
+                               cuda_array<float, 2>* projection,
+                               cuda_array<float, 2>* view,
+                               size_t width,
+                               size_t height,
+                               float3& ray_pos,
+                               float3& ray_vec,
+                               float3& origin,
+                               float near = 0.1f
+                              ) {
+    float2 ss_norm = make_float2(((float) x) / ((float) width),
+                                 ((float) y) / ((float) height));
+    float2 clip_space = ss_norm * 2.0f - 1.0f;
     
-    size_t product = 1;
-    for (int i = 0; i < N; i++) {
-        arr->shape[i] = dims[i];
-        arr->stride[i] = product;
+    cuda_array<float, 2> proj_view;
+    float proj_view_m[4][4];
+    mat4<float>(&proj_view, proj_view_m);
+    
+    cuda_array<float, 2> viewproj_inv;
+    float viewproj_inv_m[4][4];
+    mat4<float>(&viewproj_inv, viewproj_inv_m);
+    
+    cuda_array<float, 2> view_inv;
+    float view_inv_m[4][4];
+    mat4<float>(&view_inv, view_inv_m);
+    
+    matmul_sq<float, 2>(projection, view, &proj_view);
+    invert_matrix(proj_view.data, viewproj_inv.data);
+    invert_matrix(view->data, view_inv.data);
+    
+    
+    float4 homogeneous = make_float4(
+                             index(&viewproj_inv, 0, 0) * clip_space.x
+                             + index(&viewproj_inv, 0, 1) * clip_space.y,
+                             index(&viewproj_inv, 1, 0) * clip_space.x
+                             + index(&viewproj_inv, 1, 1) * clip_space.y,
+                             0.0f,
+                             0.0f
+                         );
+                         
+    origin =
+        make_float3(
+            index(&view_inv, 0, 3), index(&view_inv, 1, 3), index(&view_inv, 2, 3)
+        );
         
-        product *= dims[i];
-    }
-    arr->num_elements = product;
-    arr->data = new T[product];
+    float4 h_div = (homogeneous / homogeneous.w);
+    float3 projected = make_float3(h_div.x, h_div.y, h_div.z) - origin;
     
-    return arr;
+    ray_vec = normalize(projected);
+    ray_pos = origin + ray_vec * near;
 }
 
-template <typename T, size_t N>
-__host__ __device__ void assign(cuda_array<T, N>* arr, T* ptr, size_t dims[N],
-                                const bool& delete_mem = false) {
-    size_t product = 1;
-    for (int i = 0; i < N; i++) {
-        arr->shape[i] = dims[i];
-        arr->stride[i] = product;
-        
-        product *= dims[i];
-    }
-    arr->num_elements = product;
-
-    if (delete_mem) {
-        delete[] arr->data;
-    }
+__device__ float3 forward_pass(int x,
+                               int y,
+                               cuda_array<float, 3>* sdf,
+                               cuda_array<float, 1>* p0,
+                               cuda_array<float, 1>* p1,
+                               cuda_array<float, 3>* sdf_shape,
+                               
+                               
+                               cuda_array<float, 2>* projection,
+                               cuda_array<float, 2>* view,
+                               cuda_array<float, 2>* ray_transform,
+                               
+                               int width,
+                               int height
+                              ) {
+    float3 ray_pos = make_float3(0.0f, 0.0f, 0.0f);
+    float3 ray_vec = make_float3(0.0f, 0.0f, 0.0f);
+    float3 origin = make_float3(0.0f, 0.0f, 0.0f);
     
-    arr->data = ptr;
-}
-
-template <typename T, size_t N>
-__host__ void delete_array(cuda_array<T, N>* array) {
-    delete[] array->data;
-    delete array;
-}
-
-template <typename T, size_t N>
-__host__ __device__ T& index(cuda_array<T, N>* arr, const int& i) {
-    return arr->data[i * arr->stride[0]];
-}
-
-template <typename T, size_t N>
-__host__ __device__ T& index(cuda_array<T, N>* arr,
-                             const int& i,
-                             const int& j) {
-    return arr->data[i * arr->stride[0] + j * arr->stride[1]];
-}
-
-template <typename T, size_t N>
-__host__ __device__ T& index(cuda_array<T, N>* arr,
-                             const int& i,
-                             const int& j,
-                             const int& k) {
-    return arr->data[i * arr->stride[0] + j * arr->stride[1] + k * arr->stride[1]];
-}
-
-float example_sphere(float x, float y, float z) {
-    return sqrtf(x * x + y * y + z * z) - 3.0f;
-}
-
-template <typename T>
-__host__ void fill(cuda_array<T, 2>* arr, const T& val) {
-    for (int j = 0; j < arr->shape[1]; j++) {
-        for (int i = 0; i < arr->shape[0]; i++) {
-            index(arr, i, j) = val;
-        }
-    }
-}
-
-template <typename T>
-__host__ void fill(cuda_array<T, 3>* arr, const T& val) {
-    for (int k = 0; k < arr->shape[2]; k++) {
-        for (int j = 0; j < arr->shape[1]; j++) {
-            for (int i = 0; i < arr->shape[0]; i++) {
-                index(arr, i, j, k) = val;
-            }
-        }
-    }
-}
-
-template <typename T>
-__host__ void gen_sdf(std::function<float(float, float, float)> func,
-                      cuda_array<T, 3>* arr) {
-    for (int k = 0; k < arr->shape[2]; k++) {
-        for (int j = 0; j < arr->shape[1]; j++) {
-            for (int i = 0; i < arr->shape[0]; i++) {
-                index(arr, i, j, k) = func(i, j, k);
-            }
-        }
-    }
-}
-
-template <typename T>
-__host__ void print(cuda_array<T, 2>* arr) {
-    for (int i = 0; i < arr->shape[0]; i++) {
-        for (int j = 0; j < arr->shape[1]; j++) {
-            std::cout << index(arr, i, j) << " ";
-        }
-        std::cout << std::endl;
-    }
-}
-
-template <typename T, size_t N>
-__host__ T* to_device(const cuda_array<T, N>* arr, size_t** shape) {
-    T* device_ptr;
+    projection_gen(x, y, projection, view, width, height, ray_pos, ray_vec, origin);
     
-    std::cout << "copying " << arr->num_elements << " to device" << std::endl;
-    cudaMalloc(&device_ptr, arr->num_elements * sizeof(T));
-    cudaMemcpy(device_ptr, arr->data, arr->num_elements * sizeof(T),
-               cudaMemcpyHostToDevice);
-               
-    cudaMalloc(shape, N * sizeof(size_t));
-    cudaMemcpy(*shape, arr->shape, N * sizeof(size_t),
-               cudaMemcpyHostToDevice);
-               
-    return device_ptr;
-}
-
-template <typename T, size_t N>
-__host__ void to_host(T* arr, cuda_array<T, N>* host) {
-    std::cout << "copying " << host->num_elements << " to host" << std::endl;
-    
-    int ret = cudaMemcpy(host->data, arr,
-                         host->num_elements * sizeof(T),
-                         cudaMemcpyDeviceToHost);
-    std::cout << "ret " << ret << std::endl;
-    cudaFree((void*) arr);
+    return ray_pos;
 }
 
 __global__
@@ -199,7 +136,7 @@ void render(float* projection_matrix_,
                      image_size);
                      
     if (i < 16) {
-        index(&projection_matrix, i % 4, i / 4) += 10.0f;
+        index(&projection_matrix, i % 4, i / 4) += (float)width;
     }
 }
 
@@ -242,7 +179,7 @@ void trace() {
     
     size_t mat4_dims[2] = {4, 4};
     size_t vec3_dims[1] = {3};
-    size_t img_dims[3] = {width, height, 3};
+    size_t img_dims[3] = {3, width, height};
     size_t single_dim[1] = {1};
     
     size_t* mat4_dims_device;
@@ -294,24 +231,31 @@ void trace() {
     float* dloss_dtransform_device = to_device<float, 2>(dloss_dtransform_host,
                                      &mat4_dims_device);
                                      
-    render <<< 64, 64>>> (projection_device,
-                          view_device,
-                          transform_device,
-                          
-                          sdf_device, n_matrix_device,
-                          p0_device, p1_device,
-                          
-                          target_device,
-                          
-                          width, height,
-                          
-                          // outputs
-                          loss_device,
-                          forward_device,
-                          dloss_dsdf_device,
-                          dloss_dtransform_device
-                         );
-                         
+    const size_t block_size = 8;
+    const size_t grid_size_x = (int)(ceil((float) width / (float) block_size));
+    const size_t grid_size_y = (int)(ceil((float) height / (float) block_size));
+    
+    dim3 blocks(grid_size_x, grid_size_y);
+    dim3 threads(block_size, block_size);
+    
+    render <<< blocks, threads>>> (projection_device,
+                                   view_device,
+                                   transform_device,
+                                   
+                                   sdf_device, n_matrix_device,
+                                   p0_device, p1_device,
+                                   
+                                   target_device,
+                                   
+                                   width, height,
+                                   
+                                   // outputs
+                                   loss_device,
+                                   forward_device,
+                                   dloss_dsdf_device,
+                                   dloss_dtransform_device
+                                  );
+                                  
     to_host<float, 2>(projection_device, projection_host);
     
     print<float>(projection_host);
