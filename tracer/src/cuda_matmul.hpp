8ec5c473d9decf3e4ce139804c72da10f19aa6d6
Srinivas Kaza <srinivas@kaza.io>
2019-08-02 09:22:44 -0400

    reorganized CUDA io-related things

--- tracer/src/cuda_matmul.hpp
+++ tracer/src/cuda_matmul.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <iostream>
+
 #include "HalideBuffer.h"
 #include "math.h"
 #include "helper_math.h"
@@ -69,25 +71,6 @@ __host__ __device__ void reinterpret(cuda_array<T, N>* arr,
     assert(product_t == product_u);
 
     assign(new_arr, (U*)(arr->data), dims, false);
-}
-
-template <typename T, size_t N>
-__host__ inline cuda_array<T, N>* from_buffer(Halide::Runtime::Buffer<T>& buf) {
-    cuda_array<T, N>* output = new cuda_array<T, N>;
-    int product = 1;
-
-    assert(buf.dimensions() == N);
-
-    for (int i = 0; i < buf.dimensions(); i++) {
-        output->shape[i] = buf.dim(i).extent();
-        output->stride[i] = buf.dim(i).stride();
-        product *= buf.dim(i).extent();
-    }
-
-    output->num_elements = product;
-    output->data = buf.data();
-
-    return output;
 }
 
 template <typename T, size_t N>
@@ -212,24 +195,6 @@ __host__ void print(cuda_array<T, 2>* arr) {
         }
         std::cout << std::endl;
     }
-}
-
-__host__ void write_img(const char* filename, cuda_array<float, 3>* arr) {
-    unsigned char* arr_bytes = (unsigned char*) malloc(arr->num_elements * sizeof(
-                                   float));
-    int n = 0;
-
-    for (int k = 0; k < arr->shape[2]; k++) {
-        for (int j = 0; j < arr->shape[1]; j++) {
-            for (int i = arr->shape[0] - 1; i >= 0; i--) {
-                float p = index(arr, i, j, k);
-                //std::cout << p << std::endl;
-                arr_bytes[n++] = ((p < 0 ? 0 : (p > 1.0 ? 255 : p)) * 255.0);
-            }
-        }
-    }
-
-    stbi_write_bmp(filename, arr->shape[1], arr->shape[2], 3, arr_bytes);
 }
 
 template <typename T, size_t N>
